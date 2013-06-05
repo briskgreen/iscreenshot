22762af09cff93fc6049c66a86f5efcd1b922e9e
briskgreen <briskgreen@163.com>
2013-06-05 21:47:23 +0800

    updata

---
+++ iscreenshot_get_root_window.h
@@ -0,0 +1,8 @@
+/*获取整个屏幕库头文件*/
+
+#ifndef _GET_ROOT_WINDOW_H
+#define _GET_ROOT_WINDOW_H
+
+iscreenshot_get_root_window(void);
+
+#endif
---
+++ main.c
@@ -0,0 +1,4 @@
+#include "iscreenshot_get_root_window.h"
+
+int main(int argc,char **argv)
+{}
