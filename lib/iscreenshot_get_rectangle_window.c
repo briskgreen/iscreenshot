ed843a5dbd63481856a830af6aba75444900d1da
briskgreen <briskgreen@163.com>
2013-06-22 23:19:42 +0800

    updata

---
+++ lib/iscreenshot_get_rectangle_window.c
@@ -0,0 +1,122 @@
+#include "iscreenshot_get_rectangle_window.h"
+
+GdkPixbuf *iscreenshot_get_rectangle_window(GdkColor *color,
+		time_t t,double opacity)
+{
+	GtkWidget *win;
+	GdkScreen *screen;
+	IS_RECT data;
+
+	gtk_init(NULL,NULL);
+
+	if(sleep(t) == -1)
+		error_quit("Sleep");
+
+	screen=gdk_screen_get_default();
+	win=gtk_window_new(GTK_WINDOW_POPUP);
+	gtk_widget_set_app_paintable(win,TRUE);
+
+	data.pressed=FALSE;
+	data.opacity=opacity;
+	gtk_widget_add_events(win,
+			GDK_BUTTON_MOTION_MASK|GDK_BUTTON_PRESS_MASK|
+			GDK_BUTTON_RELEASE_MASK);
+
+	g_signal_connect(G_OBJECT(win),"button_press_event",
+			G_CALLBACK(iscreenshot_rect_select_area_press),&data);
+	g_signal_connect(G_OBJECT(win),"button_release_event",
+			G_CALLBACK(iscreenshot_rect_select_area_release),
+			&data);
+	g_signal_connect(G_OBJECT(win),"motion_notify_event",
+			G_CALLBACK(iscreenshot_rect_select_area_move),&data);
+
+	if(color != NULL)
+		gtk_widget_modify_bg(win,GTK_STATE_NORMAL,color);
+
+	gtk_window_set_opacity(GTK_WINDOW(win),0);
+	gtk_window_resize(GTK_WINDOW(win),
+			gdk_screen_get_width(screen),
+			gdk_screen_get_height(screen));
+
+	gdk_window_set_cursor(gdk_get_default_root_window(),
+			gdk_cursor_new(GDK_CROSSHAIR));
+	gdk_flush();
+
+	gtk_widget_show_all(win);
+
+	gtk_main();
+
+	usleep(10000);
+	gdk_window_set_cursor(gdk_get_default_root_window(),
+			gdk_cursor_new(GDK_LEFT_PTR));
+	gdk_flush();
+
+	return iscreenshot_rect_select_pixbuf(gdk_get_default_root_window(),&data);
+}
+
+void iscreenshot_rect_select_area_press(GtkWidget *widget,
+		GdkEventButton *event,IS_RECT *data)
+{
+	if(data->pressed == TRUE)
+		return;
+
+	gtk_window_move(GTK_WINDOW(widget),-100,-100);
+	gtk_window_resize(GTK_WINDOW(widget),10,10);
+	gtk_window_set_opacity(GTK_WINDOW(widget),data->opacity);
+
+	data->pressed=TRUE;
+	data->x=event->x_root;
+	data->y=event->y_root;
+}
+
+void iscreenshot_rect_select_area_release(GtkWidget *widget,
+		GdkEventButton *event,IS_RECT *data)
+{
+	if(!data->pressed)
+		return;
+
+	data->width=ABS(data->x - event->x_root);
+	data->height=ABS(data->y - event->y_root);
+	data->x=MIN(data->x,event->x_root);
+	data->y=MIN(data->y,event->y_root);
+	data->pressed=FALSE;
+
+	gtk_widget_destroy(widget);
+	gtk_main_quit();
+}
+
+void iscreenshot_rect_select_area_move(GtkWidget *widget,
+		GdkEventMotion *event,IS_RECT *data)
+{
+	GdkRectangle draw;
+
+	if(!data->pressed)
+		return;
+
+	draw.width=ABS(data->x - event->x_root);
+	draw.height=ABS(data->y - event->y_root);
+	draw.x=MIN(data->x,event->x_root);
+	draw.y=MIN(data->y,event->y_root);
+
+	if(draw.width <= 0 || draw.height <= 0)
+	{
+		gtk_window_move(GTK_WINDOW(widget),-100,-100);
+		gtk_window_resize(GTK_WINDOW(widget),10,10);
+
+		return;
+	}
+
+	gtk_window_move(GTK_WINDOW(widget),draw.x,draw.y);
+	gtk_window_resize(GTK_WINDOW(widget),draw.width,draw.height);
+}
+
+GdkPixbuf *iscreenshot_rect_select_pixbuf(GdkWindow *root,IS_RECT *data)
+{
+	GdkPixbuf *pixbuf=malloc(sizeof(GdkPixbuf));
+
+	pixbuf=gdk_pixbuf_get_from_drawable(NULL,root,NULL,
+			data->x,data->y,0,0,
+			data->width,data->height);
+
+	return pixbuf;
+}
--- lib/iscreenshot_get_rectangle_window.h
+++ lib/iscreenshot_get_rectangle_window.h
@@ -5,4 +5,28 @@
 
 #include "iscreenshot.h"
 
+typedef struct
+{
+	int x;
+	int y;
+	int width;
+	int height;
+	double opacity;
+	gboolean pressed;
+}IS_RECT;
+
+GdkPixbuf *iscreenshot_get_rectangle_window(GdkColor *color,
+		time_t t,double opacity);
+
+void iscreenshot_rect_select_area_move(GtkWidget *widget,
+		GdkEventMotion *event,IS_RECT *data);
+
+void iscreenshot_rect_select_area_press(GtkWidget *widget,
+		GdkEventButton *event,IS_RECT *data);
+
+void iscreenshot_rect_select_area_release(GtkWidget *widget,
+		GdkEventButton *event,IS_RECT *data);
+
+GdkPixbuf *iscreenshot_rect_select_pixbuf(GdkWindow *root,IS_RECT *data);
+
 #endif
